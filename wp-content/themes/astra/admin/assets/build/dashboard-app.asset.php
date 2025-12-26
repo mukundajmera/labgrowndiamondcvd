68a723c0603ef2931573e86135ce6fa863e42475
mukund ajmera <mukund.ajmera@oracle.com>
2025-12-27 03:15:23 +0530

    removing plugin

--- wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
+++ wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '8a87e30ed45b874265db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'dc39a9db76aba9b7f45c');
